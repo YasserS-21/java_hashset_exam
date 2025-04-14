7285339b0f98d2d5724a9f954972d77a1f99a596
YasserS <56772311+YasserS-21@users.noreply.github.com>
2025-04-14 15:45:09 -0400

    all tests passed

--- src/main/java/org/example/UserSet.java
+++ src/main/java/org/example/UserSet.java
@@ -1,7 +1,70 @@
 package org.example;
 
+
+
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
-public class UserSet {}
+public class UserSet {
+
+    public Set<String> userHashSet;
+            
+    public UserSet() {
+        this.userHashSet = new HashSet<>();
+    }
+
+    public boolean addUser(String userName) {
+
+        if (userName == null || userName.trim().isEmpty()){
+            throw new IllegalArgumentException("Not a valid String");
+        }
+        if (containsUser(userName)) {
+            return false;
+        }
+        return userHashSet.add(userName);
+    }
+
+    public boolean containsUser(String userName) {
+        return userHashSet.contains(userName);
+    }
+
+    public int getUserCount() {
+        return userHashSet.size();
+    }
+
+    public boolean removeUser(String userName) {
+        return userHashSet.remove(userName);
+    }
+
+    public Set<String> getUsersWithPrefix(String prefix) {
+        if (prefix == null) {
+            throw new IllegalArgumentException("No NULL values");
+        }
+        Set<String> usersWithPrefix = new HashSet<>();
+        for (String user : userHashSet) {
+
+            if (user.startsWith(prefix)) {
+                usersWithPrefix.add(user);
+            }
+
+        }
+        return usersWithPrefix;
+    }
+    public boolean areAllUsersUnique(String[] users) {
+        if (users == null) {
+            throw new IllegalArgumentException("No null values");
+        }
+        UserSet testingUniqueUsers = new UserSet();
+        for (String user : users) {
+            testingUniqueUsers.addUser(user);
+        }
+        return testingUniqueUsers.getUserCount() == users.length;
+    }
+
+
+
+
+
+}
